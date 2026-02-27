87b3b2bedc4c5b190d09f7989231654ad61ce5fc
Petr Malášek <petr.malasek@malachis.eu>
2026-02-27 19:02:39 +0100

    cleanup

--- 1-Seed/app/lib-commonjs/components/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.App = void 0;
-var App_1 = require("./App");
-Object.defineProperty(exports, "App", { enumerable: true, get: function () { return App_1.App; } });
-//# sourceMappingURL=index.js.map
--- 2-Seedling/app/lib-commonjs/hooks/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useOneDrive = void 0;
-var useOneDrive_1 = require("./useOneDrive");
-Object.defineProperty(exports, "useOneDrive", { enumerable: true, get: function () { return useOneDrive_1.useOneDrive; } });
-//# sourceMappingURL=index.js.map
